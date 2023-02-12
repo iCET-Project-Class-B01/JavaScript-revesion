c98129fa0d60ce21593a07ea9d54f7b08ee0e73f
NaashNix <thilakshanaravindu@gmail.com>
2023-02-12 13:19:58 +0530

    Sample form

--- Assignment_01/script.js
+++ Assignment_01/script.js
@@ -1,3 +1,21 @@
+const customerPage = document.getElementById('customerPage');
+const itemListPage = document.getElementById('itemListPage');
+const POSDashboard = document.getElementById('POSDashboard');
 
+customerPage.style.display = 'none';
+itemListPage.style.display = 'none';
 
+function posClicked(){
+   POSDashboard.style.display = 'block';
+   customerPage.style.display = 'none';
+}
 
+function customerClicked(){
+   POSDashboard.style.display = 'none';
+   customerPage.style.display = 'block';
+}
+
+function stockClicked(){
+   itemListPage.style.display = 'block';
+   POSDashboard.style.display = 'none';
+}
