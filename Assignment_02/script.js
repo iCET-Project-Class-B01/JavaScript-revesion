3ca43ee36f6031be583370a0f025439650a4a8e7
NaashNix <thilakshanaravindu@gmail.com>
2023-02-12 12:55:13 +0530

    Sample form

---
+++ Assignment_02/script.js
@@ -0,0 +1,13 @@
+const nameField = document.getElementById('nameField');
+const telephone = document.getElementById('telephone');
+const address = document.getElementById('address');
+const nameLabel = document.getElementById('lbl1')
+const telephoneLabel = document.getElementById('lbl2')
+const addressLabel = document.getElementById('lbl3')
+
+function saveButtonClicked() {
+   console.log(nameField.value);
+   console.log(telephone.value);
+   console.log(address.value);
+
+}
